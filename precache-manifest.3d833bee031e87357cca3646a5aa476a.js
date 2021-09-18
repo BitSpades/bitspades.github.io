d1f7c3e06cc9a1504d606476fcaa31c1f9062054
Tundra Fizz <coleman.leif@gmail.com>
2021-09-17 19:34:17 -0700

    Updates

---
+++ precache-manifest.3d833bee031e87357cca3646a5aa476a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b9b0b1acba78be193e",
+    "url": "/css/app.cd5d276e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b7f6132478f84efc8ea8b6fcee93025d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3b4fc58700026b9098",
+    "url": "/js/about.c4e4d8bf.js"
+  },
+  {
+    "revision": "00b9b0b1acba78be193e",
+    "url": "/js/app.5dcd231e.js"
+  },
+  {
+    "revision": "0999f66a8fee5f4f50f4",
+    "url": "/js/chunk-vendors.f6b648ef.js"
+  },
+  {
+    "revision": "ecc2e9a2075f6a1b4c7be625498ee3c9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,34 @@
-"use strict";
-// exports.__esModule = true;
-var register_service_worker_1 = require("register-service-worker");
-(0, register_service_worker_1.register)(process.env.BASE_URL + "service-worker.js", {
-    ready: function () {
-        console.log("EVENT: ready()");
-    },
-    registered: function () {
-        console.log("EVENT: registered()");
-    },
-    cached: function () {
-        console.log("EVENT: cached()");
-    },
-    updatefound: function () {
-        console.log("EVENT: updatefound()");
-    },
-    updated: function (registration) {
-        console.log("EVENT: updated()");
-        // New content is available, refresh the page
-        console.log("====================");
-        caches.keys().then(function (names) {
-            for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
-                var name_1 = names_1[_i];
-                console.log(name_1);
-                caches["delete"](name_1);
-            }
-        });
-        console.log("====================");
-        console.log("Updating...");
-        registration.update();
-        console.log("...updating complete, please refresh the page");
-        // location.reload();
-    },
-    offline: function () {
-        console.log("EVENT: offline()");
-    },
-    error: function (error) {
-        console.log("EVENT: error()");
-        console.log(error);
-        // console.error("Error during service worker registration:", error);
-    }
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3d833bee031e87357cca3646a5aa476a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-books-pwa"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
